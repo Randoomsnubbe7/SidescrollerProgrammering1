b832ccafadbee31d0c8c8d9a05877de032c48a88
Oliver Åhsberg <oliver.ahsberg@learnet.se>
2023-11-07 10:55:17 +0100

    Added Enemy collision

---
+++ Assets/scripts/EnemyGoomba.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGoomba : MonoBehaviour
+{
+      public Rigidbody2D myRigidBody = null;
+
+
+
+    public float MovementSpeedperSecond = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void FixedUpdate()
+    {
+        Vector3 characterVelocity = myRigidBody.velocity;
+        characterVelocity.x = 0;
+
+        characterVelocity += MovementSpeedperSecond * transform.right.normalized;
+        myRigidBody.velocity = characterVelocity;
+    }
+}
---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadScene(string aSceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(aSceneName);
+
+    }
+}
+    
